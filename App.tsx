6f318fe9be35ca5be8eb3c5ddb17986073ce977e
asteriskzuo <zuoyuhsywn@hotmail.com>
2024-06-17 19:56:42 +0800

    chore: update android

--- App.tsx
+++ App.tsx
@@ -1,9 +1,21 @@
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { StyleSheet, Text, View } from "react-native";
+import { ChatClient, ChatOptions } from "react-native-agora-chat";
 
 export default function App() {
+  React.useEffect(() => {
+    ChatClient.getInstance().init(
+      new ChatOptions({
+        debugModel: true,
+        appKey: "sdf",
+      })
+    ).then().catch((e) => {
+      console.log('test:', e);
+    });
+  }, []);
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Open up App.tsx to start working on your app2!</Text>
     </View>
   );
 }
@@ -11,8 +23,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
