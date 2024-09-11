0421a4927a7a595f1ab168cd72f18699cee8f11e
asteriskzuo <zuoyuhsywn@hotmail.com>
2024-09-11 15:29:19 +0800

    test eas

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { StyleSheet, Text, View } from "react-native";
-import { ChatClient, ChatOptions } from "react-native-agora-chat";
+import { ChatClient, ChatOptions } from "react-native-chat-sdk";
 
 export default function App() {
   React.useEffect(() => {
---
+++ scripts/install_cmake.sh
@@ -0,0 +1,1 @@
+cp xx xxx
